7dcdb090acaa07a341d8b334327bcd3520298a1d
Caio Bakerolov <caiobakerolov@icloud.com>
2024-03-11 16:15:14 +1000

    adjust skills

--- src/containers/About/styles.ts
+++ src/containers/About/styles.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 export const GitHubSection = styled.div`
   margin-bottom: 32px;
   margin-top: 64px;
+  display: flex;
+  justify-content: center;
 
   img {
     height: 157px;
